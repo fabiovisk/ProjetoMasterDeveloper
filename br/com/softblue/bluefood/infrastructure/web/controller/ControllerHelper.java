a1b1cb6b6be32233863e8e2c24c39896a9f0cddf
Fabio <32871085+fabiovisk@users.noreply.github.com>
2020-05-29 10:42:21 -0300

    BluefoodProject

---
+++ br/com/softblue/bluefood/infrastructure/web/controller/ControllerHelper.java
@@ -0,0 +1,24 @@
+package br.com.softblue.bluefood.infrastructure.web.controller;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import br.com.softblue.bluefood.domain.restaurante.CategoriaRestaurante;
+import br.com.softblue.bluefood.domain.restaurante.CategoriaRestauranteRepository;
+
+@Controller
+public class ControllerHelper {
+	
+	public static void setEditMode(Model model, boolean isEdit) {
+		model.addAttribute("editMode", isEdit);
+	}
+	
+	public static void addCategoriasToRequest(CategoriaRestauranteRepository repository, Model model) {
+		List<CategoriaRestaurante> categorias = repository.findAll(Sort.by("nome"));
+		model.addAttribute("categorias", categorias);
+	}
+
+}
